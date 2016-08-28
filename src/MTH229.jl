a7676feb8af3d965faeeeac82d87a25bc1ca41d9
jverzani <jverzani@gmail.com>
2016-08-28 10:17:54 -0400

    use plotlyjs again

--- src/MTH229.jl
+++ src/MTH229.jl
@@ -30,7 +30,7 @@ using Reexport
 using ForwardDiff
 @reexport using SymPy
 
-Plots.gr()                          # choose as default
+Plots.plotlyjs()                          # choose as default
 
 ### 
 export tangent, secant
